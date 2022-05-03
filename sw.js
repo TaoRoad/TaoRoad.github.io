b16d06a721921836ff87fe02ce7e7c4e455e5c44
罗文韬 <9979276+lonelyforworld@user.noreply.gitee.com>
2022-05-03 23:48:16 +0800

    2022-5-3

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220502.2349';
+const cacheName = 'chirpy-20220503.2347';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
